bbc7713b0ca799acab848fb48606db53fa03b358
sob-alex <mr.sobalex@gmail.com>
2024-09-11 21:24:51 +0700

    GYMTL-7: customize primeflex breakpoints

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import "/node_modules/primeflex/primeflex.css";
 import 'primeicons/primeicons.css';
-import './index.scss'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
